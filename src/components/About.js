b23b2daacbeca69875779821ef901e2f9be6d4c6
Novizh <novizhreload1@gmail.com>
2019-10-24 11:44:31 +0700

    Configured React Router

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    )
+}
+
+export default About;
