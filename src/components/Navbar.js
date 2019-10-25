8fbbcb094485adff0b7028869b74b0fe718d208d
Novizh <novizhreload1@gmail.com>
2019-10-25 10:26:54 +0700

    Delete post action dispatch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = (props) => {
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a href="#" className="brand-logo left">Landing Page</a>
+                <a className="brand-logo left">Landing Page</a>
                 <ul className="right">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
